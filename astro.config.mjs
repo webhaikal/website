1189ff7e6b5b4889aef9f1a7381ae71ab6d82d24
Sera <haikalizz05@gmail.com>
2026-04-25 12:18:45 +0800

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,31 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
 
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import mdx from '@astrojs/mdx';
+import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
-  vite: {
-    plugins: [tailwindcss()]
+	site: import.meta.env.PROD ? "https://itssera.dev" : "http://localhost:4321",
+	integrations: [react(), mdx(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
 	},
-	fonts: [{
-		provider: fontProviders.fontsource(),
-		name: "Geist Mono",
-		cssVariable: "--font-geistmono"
-	},{
-		provider: fontProviders.fontsource(),
-		name: "Geist",
-		cssVariable: "--font-geist"
-	}]
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: "Geist Mono",
+			cssVariable: "--font-geistmono",
+		},
+		{
+			provider: fontProviders.fontsource(),
+			name: "Geist",
+			cssVariable: "--font-geist",
+		},
+	],
+
+	adapter: cloudflare(),
 });
